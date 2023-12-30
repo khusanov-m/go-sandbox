5d2684c80e3a989cdf2cc17a09b3b97641bdba2d
khusanov-m <khusanov.it.tech@gmail.com>
2023-12-30 13:38:07 +0500

    feat: add calculator project

--- calculator/main.go
+++ calculator/main.go
@@ -1,20 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	// "go-sandbox.uz/calculator-project/cmdmanager"
+	"go-sandbox.uz/calculator-project/filemanager"
+	"go-sandbox.uz/calculator-project/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make((map[float64][]float64))
-
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for idx, price := range prices {
-			taxIncludedPrices[idx] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
-	}
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
 
-	fmt.Println(result)
+		if err != nil {
+			fmt.Println("Could not process job.")
+			fmt.Println(err)
+		}
+	}
 }
--- calculator/prices/prices.go
+++ calculator/prices/prices.go
@@ -1,25 +1,57 @@
 package prices
 
-import "fmt"
+import (
+	"fmt"
+
+	"go-sandbox.uz/calculator-project/conversion"
+	"go-sandbox.uz/calculator-project/iomanager"
+)
 
 type TaxIncludedPriceJob struct {
-	TaxRate           float64
-	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
+	IOManager         iomanager.IOManager `json:"-"`
+	TaxRate           float64             `json:"tax_rate"`
+	InputPrices       []float64           `json:"input_prices"`
+	TaxIncludedPrices map[string]string   `json:"tax_included_prices"`
 }
 
-func (job TaxIncludedPriceJob) Process() {
-	result := make(map[string]float64)
+func (job *TaxIncludedPriceJob) LoadData() error {
+	lines, err := job.IOManager.ReadLines()
+
+	if err != nil {
+		return err
+	}
+
+	prices, err := conversion.StringsToFloats(lines)
+
+	if err != nil {
+		return err
+	}
+
+	job.InputPrices = prices
+	return nil
+}
+
+func (job *TaxIncludedPriceJob) Process() error {
+	err := job.LoadData()
+
+	if err != nil {
+		return err
+	}
+
+	result := make(map[string]string)
 
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrice := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
-	result[taxRate] = taxIncludedPrices
 
+	job.TaxIncludedPrices = result
+	return job.IOManager.WriteResult(job)
 }
 
-func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
+		IOManager:   iom,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
